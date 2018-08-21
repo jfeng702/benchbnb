bb3174649c44deca238b304ef34db1ab58653984
Joey Feng <jfeng702@gmail.com>
2018-08-21 14:34:58 -0700

    update

--- benchbnb/webpack.config.js
+++ benchbnb/webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: "frontend/bench_bnb.jsx",
+  entry: "./frontend/bench_bnb.jsx",
   output: {
       path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
       filename: "bundle.js"
